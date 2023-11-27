07eb9c415f44c308942a3877f0a8704f5b02cbb9
Adrian Chege <achege116@gmail.com>
2023-11-27 23:02:00 +0300

    Last digit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,6 +1,5 @@
 import random
 
-# Generate a random number between -10000 and 10000 (inclusive)
 number = random.randint(-10000, 10000)
 
 
@@ -9,17 +8,13 @@ if number >= 0:
 else:
     last_digit = -(-number % 10)
     
-#  an empty string to hold the classification
-classification = ""
-
 if last_digit > 5:
-    classification = "and is greater than 5"
+    print("Last digit of", number, "is", last_digit, "and is greater than 5")
 
-if last_digit == 0:
-    classification = "and is 0"
+elif last_digit == 0:
+    print("Last digit of", number, "is", last_digit, "and is 0") 
 
-if last_digit < 6 and last_digit != 0:
-    classification = "and is less than 6 and not 0"
-
-# Print the result
-print("Last digit of {} is {} {}".format(number, last_digit, classification))
+elif last_digit < 6 and last_digit != 0:
+    print("Last digit of", number, "is", last_digit, "and is less than 6 and not 0")
+else:
+    TypeError
