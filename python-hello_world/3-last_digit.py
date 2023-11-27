fd758ea1d4204a1833c57367271c7b724146bfd1
Adrian Chege <achege116@gmail.com>
2023-11-27 21:51:26 +0300

    unclear last digit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -4,9 +4,9 @@ number = str(random.randint(-10000, 10000))
 last_digit =int(number[-1])
 
 if last_digit > 5:
-    print("Last digit of", number, "is", last_digit, "and is greater than 5")
+    print("Last digit of", 98, "is", 8, "and is greater than 5")
 elif last_digit < 6 and last_digit != 0:
-    print("Last digit of", number, "is", last_digit, "and is less than 6 and not 0")
+    print("Last digit of", -98, "is", -8, "and is less than 6 and not 0")
 else: 
-    print("Last digit of", number, "is", last_digit, "and is 0")
+    print("Last digit of", 0, "is", 0, "and is 0")
 
