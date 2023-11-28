9098f233e9ab56a95c6b690d3d8a6c1ebe1af5de
Adrian Chege <achege116@gmail.com>
2023-11-28 22:20:27 +0300

    String reverse

---
+++ python-functions/3-string.py
@@ -0,0 +1,6 @@
+# function called reverse_string that takes a string as input and returns the reverse of that string.
+def reverse_string(word):
+    word_reverse= word[::-1]
+    return word_reverse
+
+
