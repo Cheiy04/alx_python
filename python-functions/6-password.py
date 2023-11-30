a56acc7ad2dc792e3146c57db516f5877f69d804
Adrian Chege <achege116@gmail.com>
2023-11-30 18:37:49 +0300

    Password validater correction

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -1,30 +1,23 @@
-# function called validate_password that takes a password as input
 def validate_password(password):
-
     if len(password) < 8:
         return False
-    
-    return True
-    
-    has_upper = False
-    has_lower = False
+
+    has_uppercase = False
+    has_lowercase = False
     has_digit = False
-    has_space = True
+    has_space = False
 
     for i in password:
         if i.isupper():
-            has_upper = True
+            has_uppercase = True
         elif i.islower():
-            has_lower = True
+            has_lowercase = True
         elif i.isdigit():
             has_digit = True
         elif i.isspace():
-            has_space = False
-            
-    if not has_digit or not has_space or not has_lower or not has_upper:
+            has_space = True
+
+    if not has_uppercase or not has_lowercase or not has_digit or has_space:
         return False
 
-    
-
-p= validate_password("LK0jjjhhjhjh")
-print(p)
+    return True
