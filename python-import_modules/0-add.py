e1cfd69db7fb4928c3fb5299fe51a662a96d0c2c
Adrian Chege <achege116@gmail.com>
2023-12-02 18:20:06 +0300

    import  module correction

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,11 @@
-if __name__ == '__main__':
+# Define variables a and b
+if __name__ == "__main__":
+    a = 1
+    b = 2
+
+    # Import the add function from add_0.py
     from add_0 import add
 
-    a= 1
-    b=2
-
+    # Perform the addition and print the result
     result = add(a, b)
     print("{} + {} = {}".format(a, b, result))
