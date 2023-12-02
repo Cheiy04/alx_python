d160f3775818d1596b1a937c1bbd3f475899c3d8
Adrian Chege <achege116@gmail.com>
2023-12-02 19:39:36 +0300

    arguments correction

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -2,7 +2,7 @@ if __name__ == "__main__":
 
     from args_1 import args
 
-    index = ["Hello", "Holberton", "School", "98", "Battery", "street"]
+    index = []
 
     result = args(index)
     print(result)
