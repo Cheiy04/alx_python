0e36a2b24d9ae8ab9eb197868a84a11065fccd16
Adrian Chege <achege116@gmail.com>
2023-12-03 22:55:20 +0300

    returning a string or integer

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -1,8 +1,8 @@
 if __name__ == '__main__':
     from variable_load_2 import convert_to_integer_or_string
 
-    a = "dfgh"  # Input as a string
+    value = "7"  # Input as a string
 
-    result = convert_to_integer_or_string(a)
+    result = convert_to_integer_or_string(value)
     print(result)
     print(type(result))
