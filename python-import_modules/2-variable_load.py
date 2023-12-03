308aa3b57ac22417dbd6e396d5e2df85043668f0
Adrian Chege <achege116@gmail.com>
2023-12-03 22:52:41 +0300

    returning a string

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -1,8 +1,8 @@
 if __name__ == '__main__':
+    from variable_load_2 import convert_to_integer_or_string
 
-    from variable_load_2 import string
+    a = "dfgh"  # Input as a string
 
-    a = "ssdf"
-
-    result = string(a)
+    result = convert_to_integer_or_string(a)
     print(result)
+    print(type(result))
--- python-import_modules/variable_load_2.py
+++ python-import_modules/variable_load_2.py
@@ -1,3 +1,11 @@
-def string(a):
-   
-    return a
+def convert_to_integer_or_string(value):
+    if isinstance(value, str):
+        if value.isdigit():  # Checking if the string contains only digits
+            return int(value)  
+        else:
+            return value
+    elif isinstance(value, int):
+        return value
+    else:
+        return "Unsupported type"
+
