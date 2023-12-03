1db6397e9a1e2e9a94f61d86267270a9fd6e638f
Adrian Chege <achege116@gmail.com>
2023-12-03 23:01:04 +0300

    correction

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -1,7 +1,7 @@
 if __name__ == '__main__':
     from variable_load_2 import convert_to_integer_or_string
 
-    value = ""  # Input as a string
+    value = "89"  # Input as a string
 
     result = convert_to_integer_or_string(value)
     print(result)
