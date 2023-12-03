3081b1f31a9dd13c7cc191d60b6c288401c09fee
Adrian Chege <achege116@gmail.com>
2023-12-03 22:57:45 +0300

    returning strings or intergers

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -1,7 +1,7 @@
 if __name__ == '__main__':
     from variable_load_2 import convert_to_integer_or_string
 
-    value = "7"  # Input as a string
+    value = ""  # Input as a string
 
     result = convert_to_integer_or_string(value)
     print(result)
