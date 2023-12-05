64a9e7ffca5a2a954fe8ea40b9a7d06051c2e51a
Adrian Chege <achege116@gmail.com>
2023-12-05 18:38:44 +0300

    matrices

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,15 +1,20 @@
 def print_matrix_integer(matrix=[[]]):
     result = []
-    for i in matrix:
-        result += i
-    return result
+    for row_index, row in enumerate(matrix):
+        result.extend(row)
+        if row_index != len(matrix) - 1:  # Add newline if not the last row
+            result.append('\n')
+
+    # Convert integers to strings except for '\n'
+    return " ".join("{:d}".format(item) if isinstance(item, int) else item for item in result)
 
 if __name__ == '__main__':
     matrix = [
-            [1, 2, 3],
-            [4, 5, 6],
-            [7, 8, 9],
-        ]
-    result = print_matrix_integer([matrix])
-    print("{}\n{}\n{}".format(matrix[0], matrix[1], matrix[2]))
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+    ]
+    result = print_matrix_integer(matrix)
+    print(result)
     print("--")
+
