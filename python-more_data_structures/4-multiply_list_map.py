930b290b1f8c0b58728e996e089e01ebb05dc415
Adrian Chege <achege116@gmail.com>
2023-12-06 20:30:31 +0300

    Multipy using map function

--- python-more_data_structures/4-multiply_list_map.py
+++ python-more_data_structures/4-multiply_list_map.py
@@ -1,3 +1,9 @@
 def multiply_list_map(my_list=[], number=0):
+    return my_list * 2
+
+if __name__ == '__main__':
+    numbers = [1, 2, 4, 5]
+    my_list = multiply_list_map(numbers)
     result = map(multiply_list_map, numbers)
-    return result * 2
+    print(numbers)
+    print(list(result))
