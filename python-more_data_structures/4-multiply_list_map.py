0858fce66a97214d1835012c248d54d379a9e630
Adrian Chege <achege116@gmail.com>
2023-12-06 20:33:10 +0300

    using mapping function

--- python-more_data_structures/4-multiply_list_map.py
+++ python-more_data_structures/4-multiply_list_map.py
@@ -1,9 +1,2 @@
 def multiply_list_map(my_list=[], number=0):
-    return my_list * 2
-
-if __name__ == '__main__':
-    numbers = [1, 2, 4, 5]
-    my_list = multiply_list_map(numbers)
-    result = map(multiply_list_map, numbers)
-    print(numbers)
-    print(list(result))
+    return list(map(lambda x: x * number, my_list))
