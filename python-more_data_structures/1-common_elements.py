1a87476895a7bc10799f93e27ebba875cce24a66
Adrian Chege <achege116@gmail.com>
2023-12-06 19:13:57 +0300

    Return duplicate items

--- python-more_data_structures/1-common_elements.py
+++ python-more_data_structures/1-common_elements.py
@@ -7,7 +7,9 @@ def common_elements(set_1, set_2):
 
     return duplicate
 
-set_1 = { "Python", "C", "Javascript" }
-set_2 = { "Bash", "C", "Ruby", "Perl" }
-c_set = common_elements(set_1, set_2)
-print(list(c_set))
+if __name__ == '__main__':
+
+    set_1 = { "Python", "C", "Javascript" }
+    set_2 = { "Bash", "C", "Ruby", "Perl" }
+    c_set = common_elements(set_1, set_2)
+    print(list(c_set))
