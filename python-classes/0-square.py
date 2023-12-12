d0e1f33e8794a845fe1239ecdd3fe1d58126ba4a
Adrian Chege <achege116@gmail.com>
2023-12-12 18:55:17 +0300

    Squares using classes

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,26 @@
 class Square:
-    def __init__(self, size ):
+    """A class representing a square.
+
+    The Square class is designed to create square objects with a specified size.
+    It initializes the size attribute during object creation.
+
+    Attributes:
+        We are using a private attribute that allocates the size.
+
+    Methods:
+        Only the __init__  method has been utilised in this class
+
+    Results of the class should be as follows:
+        >>> square = Square(5)
+        >>> print(square.__size)
+        5
+    """
+    def __init__(self, size):
+        """Initialize a Square object with a specified size.
+
+        Args:
+            size: The size of the square.
+        """
         self.size = size
 
 if __name__ == '__main__':
