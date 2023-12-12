90eb2e91bbad55bdabd3b2032abe5ff5bd991180
Adrian Chege <achege116@gmail.com>
2023-12-12 19:14:01 +0300

    Classes square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -26,17 +26,3 @@ class Square:
 my_square = Square()
 print(type(my_square))
 print(my_square.__dict__)
-
-
-if __name__ == '__main__':
-
-
-    try:
-        print(my_square.size)
-    except Exception as e:
-        print(e)
-
-    try:
-        print(my_square.__size)
-    except Exception as e:
-        print(e)
