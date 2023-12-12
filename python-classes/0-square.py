b0c30fbebcacc4c5f0ebf026dc948aa336aa2136
Adrian Chege <achege116@gmail.com>
2023-12-13 01:17:03 +0300

    square update

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -15,7 +15,7 @@ class Square:
         >>> print(square.__size)
         5
     """
-    def __init__(self, size = 3):
+    def __init__(self, size):
         """Initialize a Square object with a specified size.
 
         Args:
@@ -23,6 +23,6 @@ class Square:
         """
         self.__size = size
 
-my_square = Square()
+my_square = Square(3)
 print(type(my_square))
 print(my_square.__dict__)
