2e27e12fbfecbc034cc21d4c58a2f1852989c71b
Adrian Chege <achege116@gmail.com>
2023-12-13 19:02:30 +0300

    1-square.

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,10 +1,16 @@
-class Square:
-    def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+"""Defines a class Square"""
 
-# Test cases
+class Square:
+    """
+    Class that defines properties of square by: (based on 0-square.py).
+
+    Attributes:
+        size: size of a square (1 side).
+    """
+    def __init__(self, size):
+        """Creates new instances of square (1 side).
+
+        Args:
+            size: size of the square.
+        """
+        self.__size = size
