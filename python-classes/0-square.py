ec38cd8250609f1fc66a0ae52a0b04772257ec41
Adrian Chege <achege116@gmail.com>
2023-12-13 18:50:08 +0300

    0-square and its import

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,26 +1,7 @@
+"""Defines a class Square"""
+
+
 class Square:
-    """A class representing a square.
-
-    The Square class is designed to create square objects with a specified size.
-    It initializes the size attribute during object creation.
-
-    Attributes:
-        __size (int): The size of the square.
-
-    Methods:
-        No additional methods are provided in this Class
-
-    Example:
-        >>> square = Square(5)
-        >>> print(square.__size)
-        5
+    """Class that defines properties of square.
     """
-
-    def __init__(self, size=3):
-        """Initialize a Square object with a specified size.
-
-        Args:
-            size (int): The size of the square. Defaults to 3.
-        """
-        self.__size = size
-
+    pass
