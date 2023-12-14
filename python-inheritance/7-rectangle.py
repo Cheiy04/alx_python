646c4dbe1a51ea2031b62a7ce4f76a1599d3ef81
Adrian Chege <achege116@gmail.com>
2023-12-14 15:45:12 +0300

    7 rectangle update

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -20,7 +20,13 @@ class Rectangle(BaseGeometry):
     '''String representaion of our rectangle'''
     def __str__(self):
         return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
-    
+    '''
     def __dir__(self):
-        return sorted(['_Rectangle__height', '_Rectangle__width', '__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 'area', 'integer_validator'])
+        return sorted(['_Rectangle__height', '_Rectangle__width', '__class__', '__delattr__', '__dict__',
+                        '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', 
+                        '__hash__', '__init__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', 
+                        '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 
+                        'area', 'integer_validator'
+                        ])
+    '''
     
