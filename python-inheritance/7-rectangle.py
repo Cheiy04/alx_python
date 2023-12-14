272e55e0428ba4e6382883b90cc4dd86c6fd2e00
Adrian Chege <achege116@gmail.com>
2023-12-14 13:58:50 +0300

    7 rectangle well documented

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -1,10 +1,17 @@
+'''Importing the previous file'''
 BaseGeometry = __import__('5-base_geometry').BaseGeometry
+
+'''Creating a new class that inherits form the BaseGeometry'''
+
 class Rectangle(BaseGeometry):
+
+    '''Initializing our proparties and also initializing the inherited class'''
     def __init__(self, width, height):
         self.__width = width
         self.__height = height
         self.integer_validator("width", width)
         self.integer_validator("height", height)
+        
     '''Function that calculates the area of the rectangle'''
     def area(self):
         '''Return the area of the rectangle:  width by the height'''
