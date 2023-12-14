449e93f71557926e75a609e5f29e4777cd504d10
Adrian Chege <achege116@gmail.com>
2023-12-14 13:08:15 +0300

    5 base geometry

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,15 @@
+'''Class base geometry '''
+class BaseGeometry:
+    '''Method that raises an exceptions if not implemented'''
+    def area(self):
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        '''If value is not an integer raise a Type error'''
+        if not isinstance(value, int):
+            raise TypeError('<name> is not an integer')
+
+        '''If value is less than or equall to zero'''
+        if value <= 0:
+            raise ValueError('<name> must be greater than 0')
+        
