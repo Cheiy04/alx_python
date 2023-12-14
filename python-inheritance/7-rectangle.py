478659d3cd1e2493e6359b2e65c30602526fafe5
Adrian Chege <achege116@gmail.com>
2023-12-14 13:55:59 +0300

    7 rectangle

---
+++ python-inheritance/7-rectangle.py
@@ -0,0 +1,16 @@
+BaseGeometry = __import__('5-base_geometry').BaseGeometry
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+    '''Function that calculates the area of the rectangle'''
+    def area(self):
+        '''Return the area of the rectangle:  width by the height'''
+        return self.__width * self.__height
+
+    '''String representaion of our rectangle'''
+    def __str__(self):
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
+    
