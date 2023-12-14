f8f9a6867a506c8bd3417d85134b1b8c0fd0f1e0
Adrian Chege <achege116@gmail.com>
2023-12-14 11:59:09 +0300

    is instance of class

---
+++ python-inheritance/1-is_kind_of_class.py
@@ -0,0 +1,6 @@
+'''Define a function to check if object is an instance of a class or class it has inherited from'''
+
+def is_kind_of_class(obj, a_class):
+    '''Function checks if the obj is instance of a_class'''
+    return isinstance(obj, a_class)
+
