1ea13919f8c985d25056c39f246617b3c150effc
Adrian Chege <achege116@gmail.com>
2023-12-14 12:47:48 +0300

    4-base geometry update 1

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,6 +1,6 @@
 '''Class base geometry '''
 class BaseGeometry:
-    '''Method that raises an exception '''
+    '''Method that raises an exceptions if not implemented'''
     def area(self):
         raise Exception("area() is not implemented")
         
