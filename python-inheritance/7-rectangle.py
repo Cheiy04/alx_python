d1e45723df8ea0edc76fdc1a5b202c9fe429b112
Adrian Chege <achege116@gmail.com>
2023-12-14 15:02:21 +0300

    7 rectangle last update

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -11,7 +11,7 @@ class Rectangle(BaseGeometry):
         self.__height = height
         self.integer_validator("width", width)
         self.integer_validator("height", height)
-        
+
     '''Function that calculates the area of the rectangle'''
     def area(self):
         '''Return the area of the rectangle:  width by the height'''
@@ -20,4 +20,11 @@ class Rectangle(BaseGeometry):
     '''String representaion of our rectangle'''
     def __str__(self):
         return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
+    
+    def __dir__(self):
+        return sorted(['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', 
+                       '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', 
+                       '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', 
+                       '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 'area', 'integer_validator'
+                       ])
     
