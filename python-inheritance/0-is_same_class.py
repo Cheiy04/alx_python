af189c4cc8f0b3f247721e0fbde28c1a132fee8c
Adrian Chege <achege116@gmail.com>
2023-12-14 11:37:35 +0300

    Is same class

---
+++ python-inheritance/0-is_same_class.py
@@ -0,0 +1,8 @@
+'''Define a function is_same_class'''
+
+def is_same_class(obj, a_class):
+    '''Check whether the object is instance of class
+        returns True if it is 
+        return False if its not
+    '''
+    return type(obj) == a_class
