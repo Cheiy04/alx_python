6a81fa4f2e6d16cea0af306f678f308c8f5eec45
Adrian Chege <achege116@gmail.com>
2023-12-14 14:29:45 +0300

    8 square update 1

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -3,10 +3,13 @@ Rectangle = __import__("7-rectangle").Rectangle
 
 '''Create a Square class that inherits rectangle'''
 class Square(Rectangle):
+    
+    '''This is an initializer for the size of the square'''
     def __init__(self, size):
         self.__size = size
         self.integer_validator("size", size)
 
+    '''Functon to return our string'''
     def __str__(self):
         '''Returns a string rep of our square'''
         return "[Square] {}/{}".format(self.__size, self.__size)
