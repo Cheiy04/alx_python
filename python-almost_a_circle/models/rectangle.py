41e478dcaa3cf830bfe4e3ffdfa96ef9325630c2
Adrian Chege <achege116@gmail.com>
2023-12-18 19:31:24 +0300

    Assigning an argument to each attribute

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -102,3 +102,18 @@ class Rectangle(Base):
     '''Overiding the __str__ method'''
     def __str__(self):
         return "[Rectangle] ({}) {:d}/{:d} - {:d}/{:d}".format(self.id, self.__x, self.__y, self.__width, self.__height)
+
+    '''Adding a method that assigns an argument to each attribute'''
+    def update(self, *args):
+        '''assigns an argument to each attribute'''
+        if len(args) > 0:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.width = args[1]
+            if len(args) >= 3:
+                self.height = args[2]
+            if len(args) >= 4:
+                self.x = args[3]
+            if len(args) >= 5:
+                self.y = args[4]
