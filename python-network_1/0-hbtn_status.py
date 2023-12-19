260d7c81c353e36b22da62bdf1f0800db10c45ac
Adrian Chege <achege116@gmail.com>
2023-12-19 23:54:22 +0300

    Python script that fetches https://alu-intranet.hbtn.io/status

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+'''Import request '''
+import requests
+
+'''Takes the url and request it using the get method'''
+
+url = "https://alu-intranet.hbtn.io/status"
+response = requests.get(url)
+print("Body response:")
+print("\t- type: {}".format(type(response)))
+print("\t- content: {}".format(response))
+
+
+
+
