ea4030b3b95ad22f6c300ff22532e00a83606188
Adrian Chege <achege116@gmail.com>
2023-12-20 00:02:12 +0300

    Python script that fetches https://alu-intranet.hbtn.io/status: Update 2

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ url = "https://alu-intranet.hbtn.io/status"
 response = requests.get(url)
 print("Body response:")
 print("\t- type: {}".format(type(url)))
-print("\t- content: {}".format(response))
+print("\t- content: {}".format(response.status_code))
 
 
 
