d8856a776100bf07e303e02d972c55cfa0d57b02
Adrian Chege <achege116@gmail.com>
2023-12-20 20:52:10 +0300

    Write a Python script that takes in a URL, sends a request to the URL and displays the value of the variable: Update 6

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -9,6 +9,6 @@ url = input("Enter URL") if len(sys.argv) < 2 else sys.argv[1]
 
 response = requests.get(url=url)
 value = response.headers['X-Request-Id']
-# value = response['X-Request-Id="School"']
 
-print(value)
+if 'X-Request-Id' in value:
+    print(value)
