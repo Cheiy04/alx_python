91a3028562154cb4e00c7017ece438113a66cea6
Adrian Chege <achege116@gmail.com>
2024-01-16 18:50:23 +0300

    using kwargs to establish connection

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,11 +2,15 @@ import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-    mysql_username = argv[1]
-    mysql_pass = argv[2]
-    db_name = argv[3]
+    db_config = {
+        'host':     'localhost',
+        'user':     argv[1],
+        'passwd':   argv[2],
+        'db':       argv[3],
+        'port':     3306
+    }
 
-    conn = MySQLdb.connect(host='localhost', passwd=mysql_pass, port=3306, user=mysql_username, db=db_name)
+    conn = MySQLdb.connect(**db_config)
 
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM states ORDER BY id ASC')
