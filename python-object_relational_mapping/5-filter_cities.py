2bad437aad5407bae333b98bb3ab2a9fbafe6c91
Adrian Chege <achege116@gmail.com>
2024-01-17 23:37:37 +0300

    All cities by state: Update 4

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -24,12 +24,13 @@ if __name__ == '__main__':
             """
     cursor.execute(query, (argv[4],))
     states = cursor.fetchall()
-    # for i, state in enumerate(states):
-    #     if i == len(states)-1:
-    #         print("".join(state[0]), end=' ')
-    #     else:
-    #         print("".join(state[0]), end=', ')
-    print(", ".join(state[0]) for state in states)
+    for i, state in enumerate(states):
+        print(state[0], end='')
+        if i < len(states)-1:
+            print(', ', end='')
+        #     print("".join(state[0]), end=' ')
+        # else:
+        #     print("".join(state[0]), end=', ')
 
     cursor.close()
     conn.close()
