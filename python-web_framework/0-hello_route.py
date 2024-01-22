e237f52fefcd1eb739ce248d51ab8469561ca19e
Adrian Chege <achege116@gmail.com>
2024-01-22 22:31:59 +0300

    Flask web app: update 1

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -7,8 +7,11 @@ app = Flask(__name__)
 '''setting a url to execute our function and a function that prints an output'''
 @app.route("/")
 def hello():
+    '''Returns the following string'''
     return "Hello HBNB!"
 
+
+'''Calling the function'''
 print(hello())
 
 '''Running our application on a server'''
