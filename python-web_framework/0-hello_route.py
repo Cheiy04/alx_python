e27778ee1e13a058b0cd61ac1371008e36eba9f8
Adrian Chege <achege116@gmail.com>
2024-01-23 22:06:27 +0300

    Flask

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,24 +1,15 @@
 '''Importing Flask class from the flask module'''
 from flask import Flask
 
-'''Creating an instance of the Flask class'''
 app = Flask(__name__)
 
-'''Setting a URL route to execute our function and defining a function that prints an output'''
+
 @app.route("/")
 #function
 def hello():
-    '''
-    Function to handle requests to the root URL ("/").
-    
-    Returns:
-        str: A simple greeting string.
-    '''
-    '''Returns the following string'''
+
     return "Hello HBNB!"
 
-# Calling the hello function
 print(hello())
 
-# Running our Flask application on a server
 app.run()
