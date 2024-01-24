3262d5d1387f079198e33fb868b17f68ed179c0e
Adrian Chege <achege116@gmail.com>
2024-01-25 02:24:11 +0300

    Connecting flask to sqlachemy

--- python-web_framework/7-db_setup.py
+++ python-web_framework/7-db_setup.py
@@ -5,7 +5,7 @@ import sys
 
 # Check for command-line arguments
 if len(sys.argv) != 4:
-    print("Usage: python 8-add_retrieve_users.py <db_username> <db_password> <db_name>")
+    print("Usage: python 7-db_setup.py <db_username> <db_password> <db_name>")
     sys.exit(1)
 
 db_username = sys.argv[1]
@@ -15,28 +15,26 @@ db_host = 'localhost'
 
 app = Flask(__name__)
 
-############################# TO DO 1 ############################
+
 # Connect to the database
 app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql://{db_username}:{db_password}@{db_host}/{db_name}"
-###############################################################
+
 
 db = SQLAlchemy(app)
 
-############################  TO DO 2 ##############################
 # Define your USER Model class here
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
 
-#################################################################
 
 # Create the database tables
 def create_tables():
     with app.app_context():
         db.create_all()
 
-create_tables()  # This calls the function to create tables
+create_tables()   #Calls the function to create tables
 
 
 @app.route('/', strict_slashes=False)
