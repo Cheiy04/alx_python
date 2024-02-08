d9afd5fd5cbf97a6d070bfceb86a28defc2907dc
Adrian Chege <achege116@gmail.com>
2024-02-08 22:45:08 +0300

    Square documented

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -1,31 +1,42 @@
-'''Importing from file 7'''
-Rectangle = __import__("7-rectangle").Rectangle
-
-'''Create a Square class that inherits rectangle'''
-class Square(Rectangle):
-
-    '''This is an initializer for the size of the square'''
-    def __init__(self, size):
-        """
-        Initialize a Square instance with the specified size.
-
-        Args:
-            size (int): The size of the square.
-        """
-        self.__size = size 
-        super().__init__('size', size)
-        self.integer_validator("size", size)  # Call the constructor of the parent class (Rectangle)
-    def __str__(self):
-        """
-        Return a string representation of the square.
-
-        Returns:
-            str: The formatted string representation.
-        """
-        return "[Square] {}/{}".format(self._Rectangle__width, self._Rectangle__height)
+'''creating the class BaseGeometry'''
+class BaseGeometry:
+    '''Method that checks for exception'''
+    def area(self):
+        raise Exception('area() is not implemented')
     
+    '''Method that checks if the value is a positive integer'''
+    def integer_validator(self, name, value):
+        if type(value) != int:
+            raise TypeError('{} must be an integer'.format(name))
+        if value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
 
-s = Square(13)
+'''Creating class rectangle that inherits from BaseGeometry'''
+class Rectangle(BaseGeometry):
+    '''Constructor that initializes our values'''
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
 
-print(s)
-print(s.area())
+    '''Method that prints the output'''
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    '''Method that calculates our area'''
+    def area(self):
+        return self.__width * self.__height
+
+
+'''Creating a class Square that inherits from Rectangle'''
+class Square(Rectangle):
+    '''Constructor that initializes our values'''
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    '''Method that prints the output'''
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
