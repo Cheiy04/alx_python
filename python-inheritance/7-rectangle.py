a82bf7f20846fcf326f48d0db66b2dd6af0fdeb5
Adrian Chege <achege116@gmail.com>
2024-02-08 22:36:13 +0300

    rectangle documented

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -1,23 +1,27 @@
+'''Creating the class BaseGeometry'''
 class BaseGeometry:
+    '''Creating a method to raise an exception'''
     def area(self):
         raise Exception('area() is not implemented')
-
+    '''Checks if the value is an positive integer'''
     def integer_validator(self, name, value):
         if type(value) != int:
             raise TypeError('{} must be an integer'.format(name))
         if value <= 0:
             raise ValueError('{} must be greater than 0'.format(name))
 
-
+'''Creating the class rectangle that inherits from BaseGeometry'''
 class Rectangle(BaseGeometry):
+    '''Initializing...'''
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-
+    '''Method that prints the result'''
     def __str__(self):
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
 
+    '''Method that calculates the area'''
     def area(self):
         return self.__width * self.__height
