9683bdb015a7711e8a4b7f060899746c2c459a19
Adrian Chege <achege116@gmail.com>
2024-02-08 22:50:29 +0300

    Square inherits from Rectangle

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -35,7 +35,7 @@ class Square(Rectangle):
     def __init__(self, size):
         self.integer_validator("size", size)
         self.__size = size
-        super().__init__(self.__size, self.__size)
+        super().__init__(size, size)
 
     '''Method that prints the output'''
     def __str__(self):
