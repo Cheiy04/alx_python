0e8e67e0992ef84ff1e7809d3ce82110578e46c2
Adrian Chege <achege116@gmail.com>
2024-02-27 00:50:12 +0300

    Get data from an API

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,28 @@
+'''Importing required libs'''
+from urllib import response
+import requests
+import sys
+'''Getting the url'''
+url = "https://jsonplaceholder.typicode.com/users"
+user_id = sys.argv[1]
+endpoint1 = url + "/{}".format(user_id)
+endpoint2 = url + "/{}".format(user_id) + '/todos'
+
+response1 = requests.get(url=endpoint1 )
+response2 = requests.get(url=endpoint2 )
+
+data1 = response1.json()
+data2 = response2.json()
+
+name = data1['name']
+
+total_tasks = len(data2)
+completed = [task['title'] for task in data2 if task['completed']]
+completed_tasks = len(completed)
+
+
+print(f"Employee {name} is done with tasks({completed_tasks}/{total_tasks}):")
+for i in completed:
+    print(f"\t{i}")
+
+
