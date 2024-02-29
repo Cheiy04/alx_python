2118dfbd537d7aea57488d5ecfc54e97db239540
Adrian Chege <achege116@gmail.com>
2024-02-29 17:31:27 +0300

    Export to csv : Update 2

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,38 +1,53 @@
-'''Importing required libs'''
 import csv
 import requests
 import sys
 
-def get_employee_info(employee_id):
-    # Get employee details
+def employee_info(employee_id):
+    """Retrieve employee details and tasks from the given URL."""
+    # Construct URLs for employee details and tasks
     employee_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-    response_employee = requests.get(employee_url)
-    employee_data = response_employee.json()
+    todos_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
 
-    # Get employee's TODO list
-    todo_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
-    response_todo = requests.get(todo_url)
-    todo_list = response_todo.json()
+    # Make HTTP GET requests to fetch data
+    employee_response = requests.get(employee_url)
+    todos_response = requests.get(todos_url)
 
-    # Create CSV file
-    csv_file_path = f'{employee_id}.csv'
-    with open(csv_file_path, mode='w', newline='', encoding='utf-8') as csv_file:
-        fieldnames = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE']
+    # Extract relevant information from the responses
+    employee_data = employee_response.json()
+    todos_data = todos_response.json()
+
+    # Initialize a list to store task details
+    tasks = []
+
+    # Extract task details
+    for task in todos_data:
+        task_details = (
+            employee_id,
+            employee_data['username'],
+            task['completed'],
+            task['title']
+        )
+        tasks.append(task_details)
+
+    """creating csv file for the empoyee"""
+    file_name = f"{employee_id}.csv"
+    with open(file_name, "w", newline='') as csv_file:
+        fieldnames = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE' ]
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
-        # Write header
+        """Write the csv headers"""
         writer.writeheader()
 
-        # Write tasks
-        for task in todo_list:
+        """Write the rows under the epecified columns"""
+        for task in todos_data:
             writer.writerow({
                 'USER_ID': employee_id,
                 'USERNAME': employee_data['username'],
-                'TASK_COMPLETED_STATUS': str(task['completed']),
+                'TASK_COMPLETED_STATUS': task['completed'],
                 'TASK_TITLE': task['title']
             })
 
 
 if __name__ == '__main__':
-   get_employee_info(int(sys.argv[1]))
+   employee_info(int(sys.argv[1]))
 
